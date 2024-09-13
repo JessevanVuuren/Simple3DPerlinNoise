c3326d5dfdb19f928f4f89fd05dddb90a5a29852
Jesse van Vuuren <s1135034@student.hsleiden.nl>
2024-09-13 20:34:00 +0200

    gif

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include "perlinNoise.h"
-#include <math.h>
 #include <raylib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
-const int screenWidth = 1000;
-const int screenHeight = 1000;
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
 
 float size = 10;
 
 int main(void) {
-    InitWindow(screenWidth, screenHeight, "Simple perlin noise in C");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple perlin noise in C");
 
     SetTargetFPS(60);
-    // srand(time(NULL));
+    srand(time(NULL));
 
     int *permutation_table = make_permutation();
 
-    Image img = GenImageColor(screenWidth, screenHeight, WHITE);
+    Image img = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
     Color *pixels = LoadImageColors(img);
     Texture2D texture = LoadTextureFromImage(img);
 
@@ -26,22 +26,19 @@ int main(void) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         
-
-        for (int y = 0; y < screenHeight; y++) {
-            for (int x = 0; x < screenWidth; x++) {
+        for (int y = 0; y < SCREEN_HEIGHT; y++) {
+            for (int x = 0; x < SCREEN_WIDTH; x++) {
                 float time = GetTime() * 0.5;
 
-                float value = Noise3D(x / (double)screenHeight * size, y / (double)screenWidth * size, time, permutation_table);
+                float value = Noise3D(x / (double)SCREEN_HEIGHT * size, y / (double)SCREEN_WIDTH * size, time, permutation_table);
                 int color = (int)floor(((value + 1) * 127.5));
 
-                pixels[y * screenWidth + x] = (Color){color, color, color, 255};
+                pixels[y * SCREEN_WIDTH + x] = (Color){color, color, color, 255};
             }
         }
 
         UpdateTexture(texture, pixels);
         DrawTexture(texture, 0, 0, WHITE);
-
-        DrawFPS(10, 10);
         EndDrawing();
     }
 
